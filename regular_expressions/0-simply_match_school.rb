68b2aaa837005bcae576e02f85ff7fcf96fab708
Joseph Nishimwe <j.nishimwe@alustudent.com>
2026-01-09 00:30:41 +0200

    Fix Task 0: match School in all input cases

--- regular_expressions/0-simply_match_school.rb
+++ regular_expressions/0-simply_match_school.rb
@@ -1,3 +1,12 @@
 #!/usr/bin/env ruby
+# 0-simply_match_school.rb
 
-puts ARGV[0].scan(/School/).join
+# Take the first argument or empty string if none
+input = ARGV[0] || ""
+
+# Use scan to find all occurrences of "School"
+matches = input.scan(/School/)
+
+# Print matches concatenated
+puts matches.join
+
